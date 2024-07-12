3f19fd78becc6efb84543dc567574107034d9a90
mirajhad <mirajhadish48@gmail.com>
2024-07-12 11:38:12 +0530

    api handling

--- src/app.js
+++ src/app.js
@@ -7,11 +7,20 @@ import morgan from 'morgan';
 const morganFormat = ':method :url :status :response-time ms';
 const app = express()
 
+const corsOptions = {
+  origin: 'http://localhost:3000/',
+  credentials: true,
+  optionSuccessStatus: 200
+}
+app.use(cors(corsOptions));
 
-app.use(cors({
-  origin: process.env.CORS_ORIGIN,
-  credentials: true
-}))
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', "http://localhost:3000");
+    res.header('Access-Control-Allow-Headers', true);
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    next();
+});
 
 app.use(express.json({limit: "100kb"}))
 app.use(express.urlencoded({extended: true, limit: "100kb"}))
