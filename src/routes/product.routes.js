acfbb9349cb157f84e792a109166b31b16a95708
mirajhad <mirajhadish48@gmail.com>
2024-07-18 14:16:45 +0530

    product edit

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -9,11 +9,16 @@ import {
   createProductReview,
   getTopProducts,
 } from '../controllers/product.controller.js';
+import { protect, admin } from '../middleware/auth.middleware.js';
+import checkObjectId from '../middleware/checkObjectId.js';
 
-
-router.route('/').get(getProducts)
+router.route('/').get(getProducts).post(protect, admin, createProduct);
+router.route('/:id/reviews').post(protect, checkObjectId, createProductReview);
+router.get('/top', getTopProducts);
 router
   .route('/:id')
-  .get(getProductById)
+  .get(checkObjectId, getProductById)
+  .put(protect, admin, checkObjectId, updateProduct)
+  .delete(protect, admin, checkObjectId, deleteProduct);
 
 export default router;
